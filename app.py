ecf789f87b968545063019afd223d5fe826f561b
Sebastian Cunningham <80603287+sebcun@users.noreply.github.com>
2025-11-19 18:47:49 +1100

    Added basic upload functionality

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+import uuid
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        uploaded = request.files.get("file")
+
+        if not uploaded or uploaded.filename == "":
+            return render_template("index.html", message="No file selected")
+
+        ext = uploaded.filename.split(".")[-1]
+
+        filename = f"{uuid.uuid4()}.{ext}"
+        path = os.path.join("uploads", filename)
+        uploaded.save(path)
+
+        return render_template("index.html", message=f"File uploaded as {filename}")
+
     return render_template("index.html")
 
 
